5b253da8b33b17fad1a6e53408bb531e0341d3e1
kinanee Samson <kinaneeSamsonJohn@gmail.com>
2021-01-15 13:20:54 +0100

    lesson-2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import Hero from './hero.js'
+
+import './style.scss'
+
+import Image from './6c4017964d32ace94c9524e320f47880.jpg'
+
+const img = document.createElement('img')
+
 const ul = document.createElement('ul')
+
+ul.className = 'hero-list'
+
+img.src = Image
+
+img.className = 'hero-img'
+
+
 console.log(Hero)
 for(let key in Hero){
     let li = document.createElement('li')
     li.textContent = key + ' - ' + Hero[key]
+    li.className = 'hero-item'
     ul.appendChild(li)
 }
+const body = document.querySelector('body')
 
-document.querySelector('body').appendChild(ul)
+body.appendChild(img)
+body.appendChild(ul)
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|jpg|jpeg|svg|gif)$/i,
+          type: 'asset/resource'
+        }
+      ]
     }
 }
